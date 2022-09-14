2237bb4c3ae312b7b39e501e8c3b966ef85240ba
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-14 10:10:08 +0530

    Add project files.

---
+++ Employeeproject/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeeproject
+{
+    internal class Employee
+    {
+        int id;
+        string name;
+        float salary;
+        public void AssignValue()
+        {
+            id = 1;
+            name = "Rahul";
+            salary = 1000;
+        }
+            public string DisplayValue()
+            {
+            return $"Employee detail:{id}/{name}/{salary}";
+            }
+        
+
+    }
+}
---
+++ Employeeproject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeeproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee project = new Employee();
+            project.AssignValue();
+            Console.WriteLine(project.DisplayValue());
+        }
+    }
+}
